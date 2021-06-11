5dd69fa5b4b95fbf19bd565a3383bb54dcad0d86
ceren esirgenci <ceren_esirgenci@hotmail.com>
2021-06-11 20:27:01 +0300

    6gün ödev3

---
+++ hrms/src/main/java/kodlama/io/hrms/api/controllers/JobTitlesController.java
@@ -0,0 +1,32 @@
+package kodlama.io.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlama.io.hrms.business.abstracts.JobTitleService;
+import kodlama.io.hrms.entities.concretes.JobTitle;
+
+@RestController
+@RequestMapping("/api/job_titles")
+
+public class JobTitlesController {
+	private JobTitleService jobTitleService;
+	
+	@Autowired
+	
+	public JobTitlesController(JobTitleService jobTitleService) {
+		super();
+		this.jobTitleService = jobTitleService;
+	}
+	@GetMapping("/getall")
+	public List<JobTitle> getAll(){
+
+        return this.jobTitleService.getAll();
+	}
+}
+	
+	
